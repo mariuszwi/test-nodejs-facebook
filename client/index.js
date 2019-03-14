01d2ac331227934701c05d6724463c4086621789
Mariusz W <mariuszwisniewski88@gmail.com>
2019-03-14 16:02:37 +0100

    First config files for docker

--- client/index.js
+++ client/index.js
@@ -4,10 +4,16 @@ const app = express();
 
 app.use("/", express.static("./dist"));
 
-// app.get('/',(reg, res)=> {
-//     console.log(req.url);
-//     res(send(JSON.stringify({status:"ok"}))
-// )});
+app.get('/',(reg, res)=> {
+    console.log(req.url);
+    res(send(JSON.stringify({status:"ok"}))
+)});
+
+app.get('/posts',(reg, res)=> {
+    fetch("http://server-service").then((posts)=>{
+        res.json(posts);
+    });
+});
 
 app.listen(3001, (err) => {
     if (err) throw err;
